24ccfc264d8ae57804476beebdbce3e6064da531
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2023-03-12 21:43:33 +0200

    map es6

--- mymixedex/public/javascript/page8.js
+++ mymixedex/public/javascript/page8.js
@@ -121,29 +121,289 @@ showObjBtn.addEventListener("click" , function(event){
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------
-//parinte Cleopa
-//https://www.youtube.com/watch?v=BiCLZ4OrxEE
+
 
 //https://www.w3schools.com/jsref/jsref_foreach.asp
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_foreach2
+
+//------forEach cu callback function myFunction
+
+let sum = 0;
+
+const numbersS = [65, 44, 12, 4];
+numbersS.forEach(myFunctions);
+
+const sumNrsParagraph=document.getElementById("sumNrsParagraph");
+sumNrsParagraph.innerText=sum;
+
+function myFunctions(item) {
+  sum += item;
+  ///is working
+}
 
 
 
 
+//------forEach cu callback function myFunction
+
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction)
+
+function myFunction(item, index, arr) {
+  arr[index] = item * 10;
+}
 
 
+const multiplyNrsParagraph=document.getElementById("multiplyNrsParagraph");
+multiplyNrsParagraph.innerText=numbers.toString();
+
+///-------------------------------------------------------------------------------------------------------------------------------------my try
 
 
+const myNrs=[5 , 10 , 20 , 30];
+const myNrsParagraph=document.getElementById("myNrsParagraph");
+myNrsParagraph.style.padding="0.5em";
+myNrsParagraph.style.margin="0.5em";
+myNrsParagraph.style.border="1px solid deeppink";
+myNrsParagraph.innerText=myNrs.toString();
+
+//---
+const yourNr=document.getElementById("yourNr");
+yourNr.style.backgroundColor="Beige";
+
+//---
+
+const pushNrBtn=document.getElementById("pushNrBtn");
+pushNrBtn.style.color="darkmagenta";
+pushNrBtn.style.fontWeight="bold";
+
+//----
+
+const sumParagraph=document.getElementById("sumParagraph");
+sumParagraph.style.padding="0.5em";
+sumParagraph.style.margin="0.5em";
+sumParagraph.style.border="1px solid indigo";
 
 
+//---event
+let mySum=0;
+sumParagraph.innerText=mySum;
+
+pushNrBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the push sum btn");
+
+    this.style.backgroundColor="MistyRose";
+    myNrs.push(yourNr.value);
+    myNrsParagraph.innerText=myNrs.join(" / ");
+
+    //--modify sum //click only once //add only one nr
+    myNrs.forEach(mySumFunction);
+
+    function mySumFunction(item){
+        return mySum += parseInt(item);
+    }
+
+    sumParagraph.innerText=mySum;
+})
+
+///--------------------------------------------------------------for loop click button only once
+
+const myNrs2=[5 , 10 , 20 , 30];
+const myNrsParagraph2=document.getElementById("myNrsParagraph2");
+myNrsParagraph2.style.padding="0.5em";
+myNrsParagraph2.style.margin="0.5em";
+myNrsParagraph2.style.border="1px solid blue";
+myNrsParagraph2.innerText=myNrs2.toString();
+
+//---
+
+const yourNrUser=document.getElementById("yourNrUser");
+yourNrUser.style.backgroundColor="Plum";
+
+//----
+
+const pushAddForLoop=document.getElementById("pushAddForLoop");
+pushAddForLoop.style.color="PaleVioletRed";
+
+//---
+const sumNrsPara=document.getElementById("sumNrsPara");
+let mySum2=0;
+sumNrsPara.innerText=mySum2;
+
+pushAddForLoop.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the add push btn");
+
+    this.style.color="SeaGreen";
+
+    //-----
+    myNrs2.push(yourNrUser.value);
+    myNrsParagraph2.innerText=myNrs2.join(" / ");
+    //-----
+
+    for(let i=0 ; i<myNrs2.length ; i++){
+        mySum2 += parseInt(myNrs2[i]);
+        console.log("mySum2" , mySum2);
+    }
+
+    //----
+    sumNrsPara.innerText=mySum2;
+})
+
+//----------------------------------------------------------------------------------for loop----------------------------------------------------------------------------------
+//https://www.w3schools.com/jsref/jsref_foreach.asp
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_foreach3
+
+const listArrNr=[8 , 6 , 12 , 7 , 5];
+
+const allelNrList=document.querySelectorAll(".elNrList");
+
+for(let i = 0 ; i < allelNrList.length ; i++){
+    allelNrList[i].style.border="1px solid red";
+    allelNrList[i].style.padding="0.5em";
+    allelNrList[i].style.margin="0.5em";
+    allelNrList[i].style.fontWeight="bold";
+    allelNrList[i].style.color="red";
+    allelNrList[i].innerText=listArrNr[i];
+}
 
 
+const  multiplyBtnElLIST=document.getElementById("multiplyBtnElLIST");
+multiplyBtnElLIST.style.color="tomato";
+
+multiplyBtnElLIST.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the multiply btn");
+
+    this.style.color="yellowgreen";
+
+    //apel functie
+    listArrNr.forEach(myMultiplyFunc);
+
+    function myMultiplyFunc(item , index , array){
+        array[index] = item * 100 ;
+    }
+
+    console.log("listArrNr" , listArrNr);
+
+    //show modification on page html
+    for(let i = 0 ; i < allelNrList.length ; i++){
+        allelNrList[i].style.border="1px solid green";
+        allelNrList[i].style.padding="0.5em";
+        allelNrList[i].style.margin="0.5em";
+        allelNrList[i].style.fontWeight="bold";
+        allelNrList[i].style.color="green";
+        allelNrList[i].innerText=listArrNr[i];
+    }
+
+});
+
+//-------------------------------------forEach-------------------------------------------------------------------------------------------------------
+
+const listNunbers=[60 , 70 , 900 , 4000 , 3];
+
+const allliNr=document.querySelectorAll(".liNr");
+
+//apel functie
+allliNr.forEach(styleListFunc);
+
+function styleListFunc(item , index , array){
+   array[index].style.color="tomato";
+   array[index].style.border="1px solid forestgreen";
+   array[index].innerText=listNunbers[index];
+}
 
 
+const divisionBtn=document.getElementById("divisionBtn");
+divisionBtn.style.color="forestgreen";
+divisionBtn.style.fontWeight="bold";
+
+divisionBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the division btn");
+
+    this.style.color="blue";
+
+    //----
+
+    //-----divide nrs arr cu functia callback myDivideFunc
+    allliNr.forEach(myDivideFunc);
+    function myDivideFunc(item , index , array){
+        array[index].innerText=listNunbers[index] / 10;
+        array[index].style.color="yellowgreen";
+    }
+
+    console.log("listNunbers is : " , listNunbers);
+
+});
 
 
+//-----------------------------------------------------------------------------------------------------------------------------------grupa 13 04 2022
+
+///"DESFASURARE" forEach (ca functie);
+//void myForeach(Array gigi, function callback)
+function myForeach(arr,callback) {
+    for(var i = 0; i < arr.length; i++){
+      callback(arr[i],i);
+    }
+}
+
+var arrX = [2,3,1,6,7,8,9];
+myForeach(arrX,function(el,idx){
+  console.log("EL : ",el, " idx: ",idx);
+})
 
 
+//---------------------------------------------------------------------------------------------------------------------------------------
 
+const arrObj2=[
+    {
+        title:"capsuni",
+        imgObj:"./images/capsuni.jpg",
+        colorObj:"red"
+    },
+    {
+        title:"spring",
+        imgObj:"./images/myPhoto.JPG",
+        colorObj:"yellow"
+    },
+    {
+        title:"bicycle",
+        imgObj:"./images/cat.JPG",
+        colorObj:"yellowgreen"
+    },
+    {
+        title:"FLOWER",
+        imgObj:"https://images.pexels.com/photos/9374423/pexels-photo-9374423.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        colorObj:"orange"
+    },
+    {
+        title:"COFFEE",
+        imgObj:"https://images.pexels.com/photos/13610934/pexels-photo-13610934.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        colorObj:"cornflowerblue"
+    }
+];
 
+const objSec=document.getElementById("objSec");
+objSec.style.backgroundColor="PapayaWhip";
 
+let textObjContent=``;
+
+arrObj2.forEach(showContectObjs);
+
+function showContectObjs(item , index){
+    textObjContent += `
+    <div class="card" style="width: 18rem; margin:0.5em">
+        <img src="${item.imgObj}" class="card-img-top" alt="obj_img_fromArr">
+        <div class="card-body">
+            <h5 class="card-title" style="color:${item.colorObj}">${item.title}</h5>
+            
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+     </div>
+    `;
+}
+
+objSec.innerHTML=textObjContent;
 
--- mymixedex/public/javascript/page9.js
+++ mymixedex/public/javascript/page9.js
@@ -1,1 +1,250 @@
 console.log("hello js 9");
+
+//13 04 2022 gr 1 discord
+
+///"DESFASURARE" map (ca functie);
+//void myMap(Array gigi, function callback)
+
+const arr=[4 , 5 , 7 , 10 , 12 , 70];
+console.log("arr is : " , arr);
+
+function myMap(arr,callback){
+    var result = [];
+    for(var i = 0; i < arr.length; i++){
+      var callBackResult = callback(arr[i],i);
+      result.push(callBackResult);
+    }
+  
+    return result;
+  }
+  
+  var arrY = [1,2,3,4,5,6,7];
+  
+  var resultMyMap = myMap(arr,function(el,i){
+    return el % 2 === 0 ? el - 1 : el + 1;
+  })
+  
+  console.log("resultMyMap",resultMyMap);
+
+  //-------------------------------------------------------------------------------------------------------------------------------------------------not working
+
+const myNumbersIn=[70 , 7 , 35 , 80 , 10 , 20 , 65];
+console.log("myNumbersIn is : " , myNumbersIn);
+
+const myNumbersParag=document.getElementById("myNumbersParag");
+myNumbersParag.innerText=myNumbersIn.toString();
+myNumbersParag.style.color="tomato";
+
+const mapNrsBtn=document.getElementById("mapNrsBtn");
+mapNrsBtn.style.color="magenta";
+
+mapNrsBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the map btn");
+
+    this.style.color="forestgreen";
+//-------------------------------------------------------
+    const newmyNumbersIn=myNumbersIn.map(myFuncAdd);
+
+    function myFuncAdd(item){
+      return item + 100;
+    }
+
+    console.log("newmyNumbersIn is : " , newmyNumbersIn);
+    myNumbersParag.innerText=newmyNumbersIn.toString();
+    myNumbersParag.style.color="green";
+})
+
+//https://www.w3schools.com/jsref/jsref_map.asp
+//-------------------------------------------------------https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_map2
+
+const myNrs=[10 , 20 , 80 , 5];
+const myNrsParagraph=document.getElementById("myNrsParagraph");
+myNrsParagraph.innerText=myNrs.toString();
+
+const newmyNrs = myNrs.map(myFuncMultiplyCallback);
+
+function myFuncMultiplyCallback(item){
+  return item * 1000;
+}
+
+myNrsParagraph.innerText=newmyNrs.toString();
+
+//--------------------------------------------------------------------------------------------------------------------------
+const allKindNrs=[6 , 8 , 9, 3 , 7];
+
+const nrIfParagraph=document.getElementById("nrIfParagraph");
+nrIfParagraph.innerText=allKindNrs.toString();
+nrIfParagraph.style.color="orange";
+
+const mapIfBtn=document.getElementById("mapIfBtn");
+mapIfBtn.style.color="tomato";
+mapIfBtn.style.fontWeight="bold";
+
+mapIfBtn.addEventListener("click" , function(event){
+  event.preventDefault();
+  console.log("you clicked the ifMap btn");
+
+  this.style.color="green";
+//---------------------
+const newallKindNrs=allKindNrs.map(myIfCallbackFunc);
+
+function myIfCallbackFunc(item){
+  return item % 2 === 0 ? item + 1000 : item - 1000;
+}
+
+nrIfParagraph.innerText=newallKindNrs.toString();
+nrIfParagraph.style.color="green";
+})
+
+//--------------------------------------------------------------------------------------------------------------------------------
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_map3
+
+const persons = [
+  {firstname : "Malcom", lastname: "Reynolds"},
+  {firstname : "Kaylee", lastname: "Frye"},
+  {firstname : "Jayne", lastname: "Cobb"}
+];
+
+console.log("persons is : " , persons);
+
+const demoObjs=document.getElementById("demoObjs");
+demoObjs.style.padding="0.5em";
+demoObjs.style.margin="0.5em";
+demoObjs.style.border="1px solid green";
+
+demoObjs.innerHTML=persons.map(getFullNameCallbackFunction);
+
+function getFullNameCallbackFunction(item){
+  return [item.firstname,item.lastname].join(" ");
+}
+
+//----------------------------------------------------------------------------------------
+
+const catsMy=[
+  {
+    index:1,
+    title:"Firu",
+    imgCat:"https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum 1",
+    color:"magenta"
+  },
+  {
+    index:2,
+    title:"Micu",
+    imgCat:"https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum 2",
+    color:"red"
+  },
+  {
+    index:3,
+    title:"Coffee",
+    imgCat:"https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum 3",
+    color:"green"
+  },
+  {
+    index:4,
+    title:"cute cat",
+    imgCat:"https://images.pexels.com/photos/669015/pexels-photo-669015.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum 4",
+    color:"yellowgreen"
+  },
+  {
+    index:5,
+    title:"basket cat",
+    imgCat:"https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum 5",
+    color:"cornflowerblue"
+  },
+];
+
+let contentCats=``;
+
+catsMy.map(showCatsCallbackFunc);
+
+function showCatsCallbackFunc(item){
+  contentCats += `
+  <div class="card" style="width: 18rem; margin:0.5em">
+  <img src="${item.imgCat}" class="card-img-top" alt="my_cat_obj">
+  <div class="card-body">
+    <h5 class="card-title">${item.title}</h5>
+    <p class="card-text">${item.textCat}</p>
+    <a href="https://www.pexels.com/ro-ro/cauta/cats/"  target="_blank" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  `;
+
+  return contentCats;
+}
+
+//-----------------
+
+const catSec=document.getElementById("catSec");
+catSec.style.backgroundColor='Khaki';
+catSec.innerHTML=contentCats;
+
+//-----------------------------------------------------------------------------------------------------
+
+let contentFood=``;
+
+const foodArticle=document.getElementById("foodArticle");
+foodArticle.innerHTML=contentFood;
+
+const foods=[
+  {
+    index:1,
+    title:"icecream",
+    imgCat:"https://images.pexels.com/photos/675439/pexels-photo-675439.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum icecream",
+    color:"cornflowerblue"
+  },
+  {
+    index:2,
+    title:"cake",
+    imgCat:"https://images.pexels.com/photos/1055272/pexels-photo-1055272.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum cake",
+    color:"gold"
+  },
+  {
+    index:3,
+    title:"pizza",
+    imgCat:"https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum pizza",
+    color:"magenta"
+  },
+  {
+    index:4,
+    title:"soup",
+    imgCat:"https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum soup",
+    color:"green"
+  },
+  {
+    index:5,
+    title:"burger",
+    imgCat:"https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    textCat:"lorem ipsum burger",
+    color:"cyan"
+  },
+];
+
+const showMapFoodBtn=document.getElementById("showMapFoodBtn");
+console.log("showMapFoodBtn is : " , showMapFoodBtn);
+showMapFoodBtn.style.color="magenta";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
