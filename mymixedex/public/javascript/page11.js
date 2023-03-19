a61d470d27b36d1ef99d217889c1a4cc503624c9
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2023-03-19 21:48:51 +0200

    sort and if else

--- mymixedex/public/javascript/page11.js
+++ mymixedex/public/javascript/page11.js
@@ -1,1 +1,134 @@
 console.log("hello page 11 js");
+
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort
+
+const fruits = ["cirese" , "zmeura" , "capsuni" , "smochine" , "Banana", "Orange", "Apple", "Mango"];
+
+const demo1sort=document.getElementById("demo1sort");
+demo1sort.innerText=fruits.toString();
+
+fruits.sort();
+
+const demo2sort=document.getElementById("demo2sort");
+demo2sort.innerText=fruits.toString();
+
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort_reverse
+
+fruits.reverse();
+
+const demoReverse=document.getElementById("demoReverse");
+demoReverse.innerText=fruits.toString();
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort_alpha
+
+
+const points = [40, 100, 1, 5, 25, 10];
+const demoPoints=document.getElementById("demoPoints");
+demoPoints.innerText=points.toString();
+
+
+function myFunction1(){
+    points.sort();
+    demoPoints.innerText=points.toString();
+}
+
+
+//------------
+
+function myFunction2(){
+        points.sort(function(a , b){
+            return a - b;
+        })
+
+        demoPoints.innerText=points.toString();
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+//https://www.w3schools.com/js/js_array_sort.asp
+
+const pointsAsc = [40, 100, 1, 5, 25, 10];
+
+const pointsAscParagraph=document.getElementById("pointsAscParagraph");
+pointsAscParagraph.innerText=pointsAsc.toString();
+
+const sortAscBtn=document.getElementById("sortAscBtn");
+sortAscBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the sort asc btn");
+    //---
+    this.style.color="darkmagenta";
+    //---
+    pointsAsc.sort(function(a , b){
+        return a - b;
+    });
+    //---
+    pointsAscParagraph.innerText=pointsAsc.toString();
+});
+
+
+const sortDescBtn=document.getElementById("sortDescBtn");
+sortDescBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log("you clicked the desc btn");
+//---
+this.style.color="olive";
+
+//---
+pointsAsc.sort(function(a , b){
+    return b - a;
+})
+//---
+pointsAscParagraph.innerText=pointsAsc.toString();
+});
+
+//grupa 1 // — 15.04.2022 19:51 discord
+
+
+const arr = [2,7,1,3,9,0,12];
+console.log("arr before sort is : " , arr);
+
+/**
+  Ia elementele 2 cate 2
+  calculeaza ce vrem noi
+  daca intorc valoare mai mare decat 0
+  inseamna ca elementele trebuie interschimbate
+*/
+//METODA SORT MODIFICA ARRAY-ul PE CARE E APLICATA
+
+arr.sort(function(a , b){
+    return b -a;
+})
+
+console.log("arr after sort is : " , arr);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const strs = ["abc","a","bc","accc","bc"];
+console.log("strs before sort is : " , strs);
+//ordonare dupa lungime
+strs.sort((a,b) => a.length - b.length);
+console.log("strs after sort is : " , strs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mymixedex/public/javascript/page12.js
+++ mymixedex/public/javascript/page12.js
@@ -1,1 +1,87 @@
 console.log("hello page 12 js");
+// if (condition1) {
+//     //  block of code to be executed if condition1 is true
+//   } else if (condition2) {
+//     //  block of code to be executed if the condition1 is false and condition2 is true
+//   } else {
+//     //  block of code to be executed if the condition1 is false and condition2 is false
+//   }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//https://www.w3schools.com/js/tryit.asp?filename=tryjs_elseif
+
+const demoTime=document.getElementById("demoTime");
+
+const time = new Date().getHours();
+let greeting;
+
+if (time < 10) {
+    greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+
+  demoTime.innerText=greeting;
+
+  //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  const userNr1=document.getElementById("userNr1");
+  console.log("userNr1 is " , userNr1);
+
+
+//paragraph nr 
+const userNrChoice=document.getElementById("userNrChoice");
+
+//btn
+
+const compareNr100btn=document.getElementById("compareNr100btn");
+compareNr100btn.addEventListener("click" , function(event){
+event.preventDefault();
+console.log("you clicked the compare btn 100");
+
+this.style.color="green";
+//----
+console.log("user value nr is : " , userNr1.value);
+//----
+if(parseInt(userNr1.value) > 100){
+    return userNrChoice.innerText="your nr is more than 100";
+}else if(parseInt(userNr1.value) === 100){
+    return userNrChoice.innerText=parseInt(userNr1.value);
+}else{
+    return userNrChoice.innerText="your nr is less than 100";
+}
+//----
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
